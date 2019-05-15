142c804666284d16c10c790c1a818cd46c5e5a7e
RichardAustinCollis <RichardAustinCollis@gmail.com>
2019-05-15 17:59:13 -0400

    VS Code Gods are not pleased

--- api.js
+++ api.js
@@ -1,7 +1,58 @@
-const baseURL = 'https://api.edamam.com/search';
-const key = '2b94ef46131dbb18f7883293d4af5645';
-const ID = 'b6853abe';
+const baseURL = 'https://pixabay.com/api/docs/';
+const key = '12477843-853b5b9c996fbf5c5df92d84c';
+const resultsPerPageAPI = "&per_page=5";
 let url;
+const searchForm = document.querySelector('.form');
+// const searchTerm = document.querySelector('');
+const submitButton = document.querySelector('submit');
+const Pictures = document.querySelector('.result')
+// var query = `&q=${}`
+const searchTerm = document.querySelector('.search')
 
+
+function fetchResults(e) {  
+  console.log(e);
+  e.preventDefault();
+  url = baseURL + '?key=' + '&q=' + `${search}` + '&image_type=photo';
+  console.log(url);
+
+
+  fetch(url).then(function(result) {
+    return result.json();
+  }).then(function(json) {
+    displayResults(json);  
+    console.log(json)
+  });}
+// fetch(url)
+// .then(result => result.json())  
+// .then(result => {
+//     console.log(result);
+//     displayPicture(result);
+// })}
+function displayPicture() {
+ 
+  var result = new Image(100, 100);
+  // result.src = 
+result.src = `${search}`;
+// document.appendChild(myImage);
+// document.body.appendChild(myImage);
+  // function displayPicture() {
+// let mynewimage = createElement('img')
+let mynewimage = document.createElement('img')
+mynewimage.setattribute('src', json.IMAGE)
+body.appendchild(mynewimage)
+
+displayResults(mynewimage)
+// displayPicture('.card')
+
+// function displayPicture() {
+//   let result = document.createElement('section');
+//   
+// }
+
+// } 
+
+}
+searchForm.addEventListener('submit', fetchResults); 
 
 
